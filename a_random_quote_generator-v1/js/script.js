0d1e52d5bd02ae8bf1830fe2a04235c49a0a807d
DevMo-13 <moniquecinatl@gmail.com>
2020-05-11 19:53:12 -0700

    Adding quote array

--- a_random_quote_generator-v1/js/script.js
+++ a_random_quote_generator-v1/js/script.js
@@ -11,6 +11,68 @@ project 1 - A Random Quote Generator
  * `quotes` array 
 ***/
 
+let quotes = [
+  {
+      quote: `"Welcome to the real world. It sucks. You're gonna love it!"`,
+      source: 'Monica',
+      citation: `Friends: "The One Where Monica Gets a Roommate" - Season 1, Episode 1`,
+      year: 1994
+  },
+  {
+      quote: `"Do. Or do not. There is no try."`,
+      source: 'Yoda',
+      citation: 'Star Wars: Episode V - The Empire Strikes Back',
+      year: 1980,
+  },
+  {
+      quote: `"We are what they grow beyond. That is the true burden of all masters."`,
+      source: 'Yoda',
+      citation: 'Star Wars: Episode VIII - The Last Jedi',
+      year: 2017,
+  },
+  {
+      quote: `"I once worked with a guy for three years and never learned his name. Best friend I ever had. We still never talk sometimes."`,
+      source: 'Ron Swanson',
+      citation: `Parks and Recreation: "Flu Season" - Season 3, Episode 2`,
+      year: 2011,
+  },
+  {
+      quote: `“Never half-ass two things. Whole-ass one thing.”`,
+      source: 'Ron Swanson',
+      citation: `Parks and Recreation: "Sweet Sixteen" - Season 4, Episode 16`,
+      year: 2012,
+  },
+  {
+      quote: `“Would I rather be feared or loved? Easy. Both. I want people to be afraid of how much they love me.”`,
+      source: 'Michael Scott',
+      citation: `The Office: "The Fight" - Season 2, Episode 6`,
+      year: 2005,
+  },
+  {
+      quote: `"I need you to be careful and efficient. And remember: If I am harsh with you, it’s only because you’re doing it wrong."`,
+      source: 'Monica',
+      citation: `Friends: "The One with Rachel's Going Away Party" - Season 10, Episode 16`,
+      year: 2004,
+  },
+  {
+      quote: `"You live only as long as the last person who remembers you."`,
+      source: 'Akecheta',
+      citation: `Westworld: "The Riddle of the Sphinx" - Season 2, Episode 4`,
+      year: 2018,
+  },
+  {
+      quote: `"I wish there was a way to know you're in the good old days before you've actually left them."`,
+      source: 'Andy Bernard',
+      citation: `The Office: "Finale " - Season 9, Episode 23`,
+      year: 2013,
+  },
+  {
+      quote: `“There is only one god and his name is Death, and there is only one thing we say to Death: ‘Not today.’”`,
+      source: 'Syrio Forel',
+      citation: `Game of Thrones: "A Golden Crown" - Season 1, Episode 6`,
+      year: 2011,
+  },
+]
 
 
 /***
