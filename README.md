1fd2983e12ea2eb76e7d32a728e0cba4c5c45cf5
DevMo-13 <moniquecinatl@gmail.com>
2020-05-12 11:59:08 -0700

    Removed console.log comments and changes interval time
    Updates made based on peer feedback.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Project 1 - Random Quote Generator
  Random Quote Generator
 
- This project uses JavaScript to display a random quote every 5 seconds and changes the background color of the page when a new quote is displayed.
+ This project uses JavaScript to display a random quote every 7.5 seconds and changes the background color of the page when a new quote is displayed.
--- a_random_quote_generator-v1/js/script.js
+++ a_random_quote_generator-v1/js/script.js
@@ -5,10 +5,7 @@ project 1 - A Random Quote Generator
 ******************************************/
 
 /** 
- * `quotes` array lists various tv / movie quotes.
- * Some years are commented out in order to 
- * highlight how the conditional statements in the
- * `printQuote` function work.  
+ * `quotes` array lists various tv / movie quotes. 
 **/
 
 let quotes = [
@@ -16,7 +13,6 @@ let quotes = [
       quote: `"Welcome to the real world. It sucks. You're gonna love it!"`,
       source: 'Monica',
       citation: `Friends: "The One Where Monica Gets a Roommate"`,
-      //year: 1994
   },
   {
       quote: `"Do. Or do not. There is no try."`,
@@ -36,7 +32,6 @@ let quotes = [
       quote: `"I once worked with a guy for three years and never learned his name. Best friend I ever had. We still never talk sometimes."`,
       source: 'Ron Swanson',
       citation: `Parks and Recreation: "Flu Season"`,
-      //year: 2011,
   },
   {
       quote: `“Never half-ass two things. Whole-ass one thing.”`,
@@ -54,7 +49,6 @@ let quotes = [
       quote: `"I need you to be careful and efficient. And remember: If I am harsh with you, it’s only because you’re doing it wrong."`,
       source: 'Monica',
       citation: `Friends: "The One with Rachel's Going Away Party"`,
-      //year: 2004,
   },
   {
       quote: `"You live only as long as the last person who remembers you."`,
@@ -72,11 +66,8 @@ let quotes = [
       quote: `“There is only one god and his name is Death, and there is only one thing we say to Death: ‘Not today.’”`,
       source: 'Syrio Forel',
       citation: `Game of Thrones: "A Golden Crown"`,
-      //year: 2011,
   },
 ]
-
-// console.log(quotes); -- use this to test / log the array
 
 /**
  * `getRandomQuote` function
@@ -86,8 +77,6 @@ let quotes = [
 function getRandomQuote() {
     const randomNumber = Math.floor( Math.random () * quotes.length);
     const randomQuoteObject = quotes[randomNumber];
-    // console.log(randomNumber); -- use this to test / log the random number generated
-    // console.log(randomQuoteObject); -- use this to test / log the random quote object
     return randomQuoteObject
 }
 
@@ -99,6 +88,8 @@ function getRandomQuote() {
  * random color in RGB format and changes the 
  * background color of the page each time the 
  * `printQuote` function is called.
+ * Note: the `getRandomBgColor` function code is adapted from 
+ * https://www.w3resource.com/javascript-exercises/javascript-math-exercise-40.php
 **/
 
 function printQuote() {
@@ -111,30 +102,18 @@ function printQuote() {
      if (randomQuote.tags !== undefined) 
         randomQuoteHTML += `<span class="tags">, (${randomQuote.tags})</span>`;
      randomQuoteHTML += `</p>`
-     //console.log(randomQuote.quote); -- use this to test / log the quote
-     //console.log(randomQuote.source); -- use this to test / log the source
-     //console.log(randomQuote.citation); -- use this to test / log the citation
-     //console.log(randomQuote.year); -- use this to test / log the year (when applicable)
-     //console.log(randomQuote.tags); -- use this to test / log the tags (when applicable)
-
-/** 
- * The following `getRandomBgColor` function code is adapted from 
- * https://www.w3resource.com/javascript-exercises/javascript-math-exercise-40.php
-**/ 
-
      function getRandomBgColor() {
         const x = Math.floor(Math.random() * 256);
         const y = Math.floor(Math.random() * 256);
         const z = Math.floor(Math.random() * 256);
         const color = `rgb(${x},${y},${z})`;
-        // console.log(color); -- use this to test / log the color output
         document.body.style.background = color;
-    }
+     }
      getRandomBgColor();
      return document.getElementById('quote-box').innerHTML = randomQuoteHTML;
 }
 
 printQuote();
-setInterval(printQuote, 5000); // -- This method runs the `printQuote` function automatically every 5 seconds.
+setInterval(printQuote, 7500); // -- This method runs the `printQuote` function automatically every 7.5 seconds.
 
 document.getElementById('load-quote').addEventListener("click", printQuote, false);
